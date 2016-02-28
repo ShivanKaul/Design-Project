cac4f93e6635175dc1bce723ea3b23de97151304
Saptaparna Nath <saptanath1@gmail.com>
2016-02-28 02:22:53 -0500

    copied to analysis

---
+++ analysis/dp_1.py
@@ -0,0 +1,22 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+from base import *
+from compare import *
+
+infile = open("C:\Users\wearable\Trevor.txt", "r")
+#infile2 = open("C:\Users\wearable\IT.txt", "r")
+oxy = []
+oxy_1 = []
+test1 = base(infile)
+test1.clear()
+oxy = test1.start()
+oxy_1 = test1.filter(0.15, 150.0, 1000.0, 0.0)
+test2 = compare(oxy, oxy_1, 14)
+test2.xavier()
+test2.clear()
+
+plt.plot(oxy, 'r')
+plt.plot(oxy_1, 'b')
+plt.show()
