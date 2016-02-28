ab2ffd2d64bbfbdaa2222ef901aceb8213c38124
Saptaparna Nath <saptanath1@gmail.com>
2016-02-28 17:12:49 -0500

    comments

--- analysis/dp_1.py
+++ analysis/dp_1.py
@@ -3,10 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 import os 
+import glob
+
+# importing class 
 from convert_csv import *
 from base import *
 from compare import *
-import glob
 
 for files in glob.glob("*.csv"):
 	infile = open(files, "r")
@@ -17,20 +19,24 @@ for files in glob.glob("*.csv"):
 		tag = 'happy'
 	else:
 		tag = 'physical'
-
+	# initiate all lists
 	oxy_1 = []
 	hea =[]
 	sk = []
 	time =[]
 	x = []
+	# call for conversion 
 	test1 = convert_csv(infile, tag)
-	test1.clear()
+	# need to do this for every file 
+	test1.clear()						
 	x = test1.generate()
 	hea = test1.heart()
 	sk = test1.skin()
 	time = test1.time()
-	oxy_1 = test1.filter(sk, q=590.0, r=50000.0, p=399900.0, k=0.0)
+	# filter 
+	oxy_1 = test1.filter(sk, q=590.0, r=50000.0, p=399900.0, k=0.0) 
 	oxy_2 = test1.filter(hea, q=0.99, r=260.0, p=1000.0, k=0.0)
+	#print the plot
 	plt.figure(1)
 	plt.subplot(211)
 	plt.plot(sk, 'r')
