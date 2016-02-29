237f147ad7a0cb8ccb6d745d724320968082f1b0
ShivanKaul <shivankaul.1993@gmail.com>
2016-02-28 19:37:28 -0500

    Generate master list of tuples

--- analysis/converter.py
+++ analysis/converter.py
@@ -1,67 +1,59 @@
-import math
-import csv 
-import os
-import matplotlib
-import numpy as np
-import matplotlib.pyplot as plt
-class Converter:   # file used to convert the csv files to give heart rate, skin and time
+import csv
 
-	hear = []
-	sk = []
-	tim = []
+# Used to convert the csv files to give heart rate, skin and time
 
-	def __init__(self, infile, tag):
-		self.infile = infile
-		self.tag = tag
-		self.clear
 
-	def generate (self):
-		line = csv.reader(self.infile)
-		data = tuple(line)
-		x = []
-		i = 0
-		j = 0 
-		while i < len(data):
+class Converter:
+    heart = []
+    skin = []
+    time = []
+    filtered_skin = []
+    filtered_heart = []
 
-			if(data[i][2] != ''):
-				if (self.tag == 'fear'):
-					x.append(data[i] + ['fear'])
-				elif (self.tag == 'happy'):
-					x.append(data[i] + ['happy'])
-				else:
-					x.append(data[i] + ['physical'])		
-			i = i+1
-		
-		while j < len(x):
-			self.hear.append(float(x[j][2]))
-			self.sk.append(float(x[j][1]))
-			self.tim.append(float(x[j][0]))
-			j = j+1
-		return 	x		
+    def __init__(self, infile, tag):
+        self.infile = infile
+        self.tag = tag
+        self.clear
 
-	def heart(self):
-		return self.hear
-	
-	def skin(self):
-		return self.sk
+    def generate(self):
 
-	def time (self):
-		return self.tim
+        file = csv.reader(self.infile, delimiter='\n')
+        for row in file:
+            # A row as converted by csv.reader is actually a single item
+            # list of string
+            # We need to get the item and split it on the comma
+            row = row[0].split(',')
+            if (row[2]):
+                row = map(float, row)
+                self.heart.append(row[2])
+                self.skin.append(row[1])
+                self.time.append(row[0])
 
-	def filter (self, mylist, q, r, p, k):
-		i = 0
-		x = mylist[0]
-		filtered = []
-		while i < len(mylist):
-			p = p + q			
-			k = p/(p + r)
-			x = x + k*(mylist[i] - x)
-			p = (1-k)*p
-			filtered.append(x)
-			i = i+1	
-		return filtered	
+        # Filter
+        self.filtered_skin = self.filter(self.skin,
+                                         q=590.0, r=50000.0, p=399900.0, k=0.0)
+        self.filtered_heart = self.filter(self.heart,
+                                          q=0.99, r=260.0, p=1000.0, k=0.0)
 
-	def clear (self):
-		self.hear[:] = []	
-		self.sk[:] = []
-		self.tim[:] = []
+        # Zip all lists. We have the tag for the file, and we replicate that
+        # into a list of the same size as the other 3 lists
+        listOfTuples = zip(self.time, self.filtered_skin,
+                           self.filtered_heart, [self.tag] * len(self.time))
+
+        return listOfTuples
+
+    def filter(self, mylist, q, r, p, k):
+        x = mylist[0]
+        filtered = []
+        for i in xrange(len(mylist)):
+            p = p + q
+            k = p/(p + r)
+            x = x + k*(mylist[i] - x)
+            p = (1-k)*p
+            filtered.append(x)
+        return filtered
+
+    def clear(self):
+        self.heart[:] = []
+        self.skin[:] = []
+        self.tim[:] = []
--- analysis/main.py
+++ analysis/main.py
@@ -1,8 +1,7 @@
-import matplotlib
-import numpy as np
+# import numpy as np
 import matplotlib.pyplot as plt
 import csv
-import os 
+import os
 import glob
 
 from converter import Converter
@@ -11,48 +10,43 @@ from compare import *
 
 listOfTuples = []
 
+
 def plot(skin, filtered_skin, heart, filtered_heart):
-	plt.figure(1)
-	plt.subplot(211)
-	plt.plot(skin, 'r')
-	plt.plot(filtered_skin, 'b')
-	plt.subplot(212)
-	plt.plot(heart, 'r')
-	plt.plot(filtered_heart, 'b')
+    plt.figure(1)
+    plt.subplot(211)
+    plt.plot(skin, 'r')
+    plt.plot(filtered_skin, 'b')
+    plt.subplot(212)
+    plt.plot(heart, 'r')
+    plt.plot(filtered_heart, 'b')
+    plt.show()
 
 # Start main script
 for file in glob.glob("*.csv"):
-	if file == "listOfTuples.csv":
-		continue
-	infile = open(file, "r")
-	print file
+    # for file in glob.glob("happy_kungfu2.csv"):
+    if file == "listOfTuples.csv":
+        continue
+    infile = open(file, "r")
+    print file
 
-	# Assign tags
-	if 'fear' in os.path.basename(infile.name):
-		tag = 'fear'
-	elif 'happy' in os.path.basename(infile.name):
-		tag = 'happy'
-	else:
-		tag = 'physical'
+    # Assign tags
+    if 'fear' in os.path.basename(infile.name):
+        tag = 'fear'
+    elif 'happy' in os.path.basename(infile.name):
+        tag = 'happy'
+    else:
+        tag = 'physical'
 
-	# Convert
-	converter = Converter(infile, tag)
+    # Convert
+    converter = Converter(infile, tag)
+    # Build up list
+    listOfTuples.append(converter.generate())
 
-	listOfTuples.append(converter.generate())
-	heart = converter.heart()
-	skin = converter.skin()
-	time = converter.time()
+    # Plot
+    plot(converter.skin, converter.filtered_skin,
+         converter.heart, converter.filtered_heart)
 
-	# Filter 
-	filtered_skin = converter.filter(skin, q=590.0, r=50000.0, p=399900.0, k=0.0) 
-	filtered_heart = converter.filter(heart, q=0.99, r=260.0, p=1000.0, k=0.0)
-
-	# Plot
-	plot(skin, filtered_skin, heart, filtered_heart)
-
-	# Print to file
-	with open("listOfTuples.csv", "w") as f:
-		writer = csv.writer(f)
-		writer.writerows(listOfTuples)
-
-
+    # Print to file
+    with open("listOfTuples.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(listOfTuples)
