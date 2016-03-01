e6d58cf5a5832ca727ca98c6238d2f0b5d1d848b
ShivanKaul <shivankaul.1993@gmail.com>
2016-03-01 02:56:38 -0500

    Do GridSearch to get hyperparameters, fix #16

--- analysis/learn.py
+++ analysis/learn.py
@@ -2,22 +2,41 @@ import numpy as np
 # from sklearn.cross_validation import cross_val_predict
 # from sklearn.cross_validation import cross_val_score
 from sklearn.cross_validation import train_test_split
-# from sklearn import linear_model
-from sklearn import svm
+from sklearn import linear_model
+from sklearn.svm import SVC
 from scipy import stats
 from scipy.stats import itemfreq
+from sklearn.cross_validation import StratifiedShuffleSplit
+
+from sklearn.grid_search import GridSearchCV
 
 TYPE_INDEX = 3
+# TYPE_INDEX = 5
 
 
 def learn_with_test(dataset, testset):
     matrix_ds = np.asarray(dataset)
-    matrix_ds_test = np.asarray(dataset)
-    clf = svm.SVC(kernel='rbf', C=5)
+    matrix_ds_test = np.asarray(testset)
+    # clf = linear_model.LogisticRegression()
     training_target = matrix_ds[:, TYPE_INDEX]
     training_dataset = matrix_ds[:, 1:TYPE_INDEX].astype(np.float)
     testing_target = matrix_ds_test[:, TYPE_INDEX]
     testing_dataset = matrix_ds_test[:, 1:TYPE_INDEX].astype(np.float)
+
+    # Parameter selection
+    # Set the parameters by cross-validation
+
+    # cv = StratifiedShuffleSplit(training_target, n_iter=5, test_size=0.2, random_state=42)
+    # C_range = 10.0 ** np.arange(-3, 3)
+    # gamma_range = 10.0 ** np.arange(-3, 3 )
+    # param_grid = dict(gamma=gamma_range, C=C_range)
+
+    # clf = GridSearchCV(SVC(), param_grid, cv=cv)
+    # clf.fit(training_dataset, training_target)
+    # print("The best parameters are %s with a score of %0.2f"
+      # % (clf.best_params_, clf.best_score_))
+    clf = SVC(kernel='rbf', C=10, gamma=0.1)
+
 
     clf.fit(training_dataset, training_target)
     # print clf.score(testing_dataset, testing_target)
@@ -40,7 +59,7 @@ def learn(dataset):
     matrix_ds = np.asarray(dataset)
     # clf = linear_model.LogisticRegression()
     # clf = linear_model.Perceptron()
-    clf = svm.SVC(kernel='rbf', C=5)
+    clf = SVC(kernel='rbf', C=10, gamma=0.1)
     target = matrix_ds[:, TYPE_INDEX]
     training_data = matrix_ds[:, 1:TYPE_INDEX].astype(np.float)
 
