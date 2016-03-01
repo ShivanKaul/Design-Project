28842fd02d842129e3604aee7a16ae79493f524e
ShivanKaul <shivankaul.1993@gmail.com>
2016-02-29 22:09:31 -0500

    Modify main code to be more modular

--- analysis/main.py
+++ analysis/main.py
@@ -7,8 +7,44 @@ import glob
 from converter import Converter
 from base import *
 from compare import *
+from learn import learn
+from learn import learn_with_test
 
-listOfTuples = []
+
+def get_tag(infile, binary=True):
+    # Assign tags: multiclass or binary
+    if binary:
+        if 'physical' in os.path.basename(infile.name):
+            tag = 'physical'
+        else:
+            tag = 'emotion'
+    else:
+        if 'fear' in os.path.basename(infile.name):
+            tag = 'fear'
+        elif 'happy' in os.path.basename(infile.name):
+            tag = 'happy'
+        elif 'sadness' in os.path.basename(infile.name):
+            tag = 'sadness'
+        elif 'tenderness' in os.path.basename(infile.name):
+            tag = 'tenderness'
+        elif 'anger' in os.path.basename(infile.name):
+            tag = 'anger'
+        elif 'physical+tenderness' in os.path.basename(infile.name):
+            tag = 'physcal+tenderness'
+        elif 'physical+happy' in os.path.basename(infile.name):
+            tag = 'physical+happy'
+        elif 'physical+fear' in os.path.basename(infile.name):
+            tag = 'physical+fear'
+        else:
+            tag = 'physical'
+    return tag
+
+
+# Print to file
+def printToFile(filename, data):
+    with open(filename, "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 
 
 def plot(skin, filtered_skin, heart, filtered_heart):
@@ -21,43 +57,29 @@ def plot(skin, filtered_skin, heart, filtered_heart):
     plt.plot(filtered_heart, 'b')
     plt.show()
 
-# Start main script
-for file in glob.glob("*.csv"):
-# for file in glob.glob("happy_Trevor.csv"):
-    if file == "listOfTuples.csv":
-        continue
-    infile = open(file, "r")
-    print file
 
-    # Assign tags
-    if 'fear' in os.path.basename(infile.name):
-        tag = 'fear'
-    elif 'happy' in os.path.basename(infile.name):
-        tag = 'happy' 
-    elif 'sadness' in os.path.basename(infile.name):
-        tag = 'sadness'
-    elif 'tenderness' in os.path.basename(infile.name): 
-        tag = 'tenderness'
-    elif 'anger' in os.path.basename(infile.name):
-        tag = 'anger'
-    elif 'physical+tenderness' in os.path.basename(infile.name):
-        tag = 'physcal+tenderness'
-    elif 'physical+happy' in os.path.basename(infile.name):
-        tag = 'physical+happy'
-    elif 'physical+fear' in os.path.basename(infile.name):
-        tag = 'physical+fear'
-    else:
-        tag = 'physical'           
+def main(matcher):
+    listOfTuples = []
+    for file in glob.glob(matcher):
+    # for file in glob.glob("happy_Trevor.csv"):
+        if file == "listOfTuples.csv" or file == "output.csv":
+            continue
+        infile = open(file, "r")
+        print file
 
-    # Convert
-    converter = Converter(infile, tag)
-    # Build up list
-    listOfTuples.append(converter.generate())
+        tag = get_tag(infile, binary=False)
+
+        # Convert
+        converter = Converter(infile, tag)
+        # Build up list
+        listOfTuples = listOfTuples + converter.generate()
+    return listOfTuples
+
+    # printToFile("listOfTuples.csv", listOfTuples)
+
+test = main("test/fear_copycat.csv")
+# learn(main("*.csv"))
+learn_with_test(main("*.csv"), test)
 
     # Plot
     # plot(converter.skin, converter.filtered_skin, converter.heart, converter.filtered_heart)
-
-# Print to file
-with open("listOfTuples.csv", "w") as f:
-    writer = csv.writer(f)
-    writer.writerows(listOfTuples)
